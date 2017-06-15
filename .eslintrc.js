97f985b87cb2d5c12d91b7426ed5b2bbe5a4dd11
Cameron <camdunne@gmail.com>
2017-06-15 09:50:33 -0400

    feat(set up): file structure

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  'env': {
+        'browser': true,
+        'meteor': true,
+        'node': true,
+        'es6': true
+    },
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ]
+};
